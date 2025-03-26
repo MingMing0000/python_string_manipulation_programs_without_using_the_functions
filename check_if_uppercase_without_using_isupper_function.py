094bbcf2e12a9cc56adef4bdf76e15fdd5322621
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-26 18:14:43 +0800

    create string and list of uppercase letters

---
+++ check_if_uppercase_without_using_isupper_function.py
@@ -0,0 +1,12 @@
+#Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+#have a string
+#create algorithm to check if all characters are in uppercase
+#use for loop to iterate through the string
+#put condition to check if the character is in uppercase
+#print the result
+
+#create a string
+string = "HELLO"
+
+#create list of uppercase characters
+uppercase_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']  

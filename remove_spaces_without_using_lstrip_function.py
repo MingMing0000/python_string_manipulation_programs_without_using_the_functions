699774db3eb6f075e9e38b3056eafa9b90a20530
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-26 16:07:36 +0800

    remove the spaces in the beginning of the string

--- remove_spaces_without_using_lstrip_function.py
+++ remove_spaces_without_using_lstrip_function.py
@@ -2,3 +2,16 @@
 #create a string
 #remove the space characters at the beginning of the string
 #display the result
+
+# create a string
+string = "   Hello, World!"
+
+# remove the space characters at the beginning of the string
+output = ""
+non_space = False
+
+for char in string:
+    if not non_space and char != " ":
+        non_space = True
+    if non_space:
+        output += char

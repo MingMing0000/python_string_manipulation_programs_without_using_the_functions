a76e26155e977fbb4be99b84b6e6d357aa2d9dbf
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-26 18:40:44 +0800

    check and print if all letters are in uppercase

--- check_if_uppercase_without_using_isupper_function.py
+++ check_if_uppercase_without_using_isupper_function.py
@@ -7,11 +7,19 @@
 
 #create a string
 string = "HELLO"
+print('word:', string)
 
 #create list of uppercase characters
 uppercase_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+uppercase_count = 0
 
-#check if each character are in uppercase
+#check if each characters are in uppercase
 for char in string:
     if char in uppercase_letters:
         uppercase_count += 1
+
+#check if all characters are in uppercase
+if uppercase_count == len(string):
+    print("All characters are in uppercase")
+else:
+    print("Not all characters are in uppercase")

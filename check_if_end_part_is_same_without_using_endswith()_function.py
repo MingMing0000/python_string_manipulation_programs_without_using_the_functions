b1be8bb0767ce47a626d853c46c28f9742613f02
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-26 18:43:25 +0800

    pseudocode

---
+++ check_if_end_part_is_same_without_using_endswith()_function.py
@@ -0,0 +1,4 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+#have a string
+#create algorithm to check if the string end part is same
+#print the result

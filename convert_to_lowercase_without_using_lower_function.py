b03d6a213a87a1a6579278005a4c5446ffee9dac
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-26 17:40:42 +0800

    pseudocode

---
+++ convert_to_lowercase_without_using_lower_function.py
@@ -0,0 +1,5 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+#have a string in upper case
+#create a dictionary with key as upper case and value as lower case
+#change the string to lower case using dictionary
+#Print the string

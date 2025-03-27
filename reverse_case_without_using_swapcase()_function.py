ea5a0c5065466e46e6baad1aae894d6ea4ade4c8
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 17:19:05 +0800

    pseudocode

---
+++ reverse_case_without_using_swapcase()_function.py
@@ -0,0 +1,4 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+#get string from user
+#use ASCII to convert the characters to uppercase or lowercase
+#display the converted string

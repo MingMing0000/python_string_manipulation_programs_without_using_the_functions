cf31ea3d64b1b30f7c4d9170355f2a948bb20e07
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 17:46:03 +0800

    get and display user input

--- titlecase_without_using_title()_function.py
+++ titlecase_without_using_title()_function.py
@@ -4,3 +4,9 @@
 #use upper() and lower() to convert the first letter of each word to uppercase and the rest to lowercase
 #join the words back to form the original string
 #display the converted string
+
+#get string from user
+input_string = input('Enter a text to convert to title case: ')
+
+#display the user input
+print('Original text:', input_string)

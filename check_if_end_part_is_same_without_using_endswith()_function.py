a9f3b4a23bebc9125226aa334391822e5d802fd4
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 15:43:33 +0800

    add functionality to get input from user

--- check_if_end_part_is_same_without_using_endswith()_function.py
+++ check_if_end_part_is_same_without_using_endswith()_function.py
@@ -4,15 +4,17 @@
 #print the result
 
 #create a string
-string = "Hello Kitty"
-print('Word:', string)
+string = input("Enter a text to check it's end part: ")
 
 #set the end part to check
-end_part = "ty"
+end_part = input('Enter some text that will be used to check if the text ends with it: ')
+
+#display to user their input
+print('Word:', string)
 print(f'Does it end with "{end_part}"?')
 
 #check if the string end part is same
 if string[-len(end_part):] == end_part:
-    print(f"Yes, the word {string} ends with {end_part}")
+    print(f"Yes, the text '{string}' ends with {end_part}")
 else:
-    print(f"No, the word {string} does not end with {end_part}")
+    print(f"No, the text '{string}' does not end with {end_part}")

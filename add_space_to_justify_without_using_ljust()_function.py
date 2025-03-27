2704740e24ed227aaff7e42b7804cb0bbbfb2aa9
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 16:15:01 +0800

    pseudocode

---
+++ add_space_to_justify_without_using_ljust()_function.py
@@ -0,0 +1,5 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+#get input from user
+#subtract the length of string to the length of the string that will be justified
+#add space characters to the end of the string
+#display the justified string

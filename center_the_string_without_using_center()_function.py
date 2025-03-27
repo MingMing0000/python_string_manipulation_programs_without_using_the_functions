a748dfde13b232c67ab61585076f67dbe4cca07a
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 16:41:07 +0800

    pseudocode

---
+++ center_the_string_without_using_center()_function.py
@@ -0,0 +1,6 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+#get input from user
+#subtract the input length of string to the length of the string that will be justified
+#divide the space length by 2
+#add space characters to the beginning and end of the string
+#display the justified string

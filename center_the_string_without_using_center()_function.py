2bccd2b6a3e0ab45997935f7e6551f2177233466
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 16:44:41 +0800

    get and display user's input

--- center_the_string_without_using_center()_function.py
+++ center_the_string_without_using_center()_function.py
@@ -4,3 +4,13 @@
 #divide the space length by 2
 #add space characters to the beginning and end of the string
 #display the justified string
+
+#get string from user
+input_string = input('Enter a text: ')
+
+#get length of string user wants
+string_length = int(input('How many characters do you want the text to be: '))
+
+#display the user input
+print(f'Original text:"{input_string}"')
+print(f'Length of text to be outputted: {string_length}')

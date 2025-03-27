d457e9edb65db8b56b8ab466ec008d832d3589ee
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 17:20:21 +0800

    get and display user input

--- reverse_case_without_using_swapcase()_function.py
+++ reverse_case_without_using_swapcase()_function.py
@@ -2,3 +2,9 @@
 #get string from user
 #use ASCII to convert the characters to uppercase or lowercase
 #display the converted string
+
+# get string from user
+input_string = input('Enter a text to reverse case: ')
+
+#display the user input
+print('Original text:', input_string)

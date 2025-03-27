2cff79c1692f7fa9dcae748ae9c7715ef1bcc186
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-27 17:45:25 +0800

    pseudocode

---
+++ titlecase_without_using_title()_function.py
@@ -0,0 +1,6 @@
+#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+#get string from user
+#split the string into words
+#use upper() and lower() to convert the first letter of each word to uppercase and the rest to lowercase
+#join the words back to form the original string
+#display the converted string

3a28adce4b79ba2e6be0adae821c7bd8ea7d0341
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-28 15:26:45 +0800

    get and display user input

--- capitalize_withou_using_capitalize()_function.py
+++ capitalize_withou_using_capitalize()_function.py
@@ -2,3 +2,9 @@
 #get string from user
 #use upper() and lower() to convert the first letter of the string to uppercase and the rest to lowercase
 #display the converted string
+
+#get string from user
+input_string = input('Enter a text to convert to capitalize: ')
+
+#display the user input
+print('Original text:', input_string)

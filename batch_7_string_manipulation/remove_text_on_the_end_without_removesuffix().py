ba48187d32d6a76704396a88283fbed3a76399bc
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 14:31:30 +0800

    remove the suffix then print the output

--- batch_7_string_manipulation/remove_text_on_the_end_without_removesuffix().py
+++ batch_7_string_manipulation/remove_text_on_the_end_without_removesuffix().py
@@ -1,6 +1,6 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 #get the string from user 
-#remove the suffix by using for loop and len() then compare the string with the suffix
+#remove the suffix by using len() then compare the string with the suffix
 #display the result
 
 #get the string from user
@@ -12,3 +12,11 @@ suffix = input('Enter the suffix you want to remove: ')
 #display the user's input
 print("Original text:", input_string)
 print("Suffix to remove:", suffix)
+
+#test if suffix is in the string
+if input_string[-len(suffix):] == suffix:
+    #remove the suffix then print
+    print(f'\nText with suffix removed: {input_string[:-len(suffix)]}')
+else:
+    #print the string
+    print(f'\nsuffix don\'t match, output text will be the same: {input_string}')

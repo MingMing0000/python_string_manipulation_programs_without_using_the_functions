1b77ceea4c9657f6d8a179c48a8cffb3fe91f54f
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 13:39:05 +0800

    pseudocode

---
+++ batch_7_string_manipulation/remove_space_on_right_without_using_rstrip().py
@@ -0,0 +1,4 @@
+#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+#get the string from user
+#remove the spaces by splitting the string and joining it again
+#display the result

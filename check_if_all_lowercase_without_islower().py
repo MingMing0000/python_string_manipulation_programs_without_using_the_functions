fdf01de8a1cbc7063cc43dacf7ae42d2ec62e7d0
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 14:55:50 +0800

    get user input

--- check_if_all_lowercase_without_islower().py
+++ check_if_all_lowercase_without_islower().py
@@ -2,3 +2,6 @@
 #get input from user
 #check if all characters are lower case using ASCII values
 #display result
+
+#get input from user
+input_string = input("Enter a text to check if all letters are in lowercase: ")

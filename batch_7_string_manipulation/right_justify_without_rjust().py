fd256faf90da7f8fb2536112085aa660e70f32bf
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 15:23:37 +0800

    pseudocode

---
+++ batch_7_string_manipulation/right_justify_without_rjust().py
@@ -0,0 +1,6 @@
+#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+#get the string from user   
+#get the number of characters from user
+#subtract the length of the string from the number of characters
+#add space characters at the beginning times the result of the subtraction
+#display the result

45807ac02688b3001c3e2228ba36faebca7019b3
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 16:17:50 +0800

    pseudocode

---
+++ batch_7_string_manipulation/count_without_using_count().py
@@ -0,0 +1,5 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+#get string from user
+#get what will be counted
+#loop through each character then count
+#display the result

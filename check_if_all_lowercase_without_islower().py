fc73034387041b9fc6f39c974cc1770474fc7c30
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 14:50:22 +0800

    pseudocode

---
+++ check_if_all_lowercase_without_islower().py
@@ -0,0 +1,4 @@
+#Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+#get input from user
+#check if all characters are lower case using ASCII values
+#display result

171dfcee5fd7c7293cd061925712cef44fdd7dec
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 15:10:57 +0800

    get and display user input

--- check_if_text_start_without_startswith().py
+++ check_if_text_start_without_startswith().py
@@ -2,3 +2,11 @@
 #get input from user
 #check if the string start with the given string
 #display the result
+
+#get input from user
+input_string = input("Enter a text: ")
+start_string = input("Enter the part to be used to check if the text starts with that part: ")
+
+#display user input
+print("Text: ", input_string)
+print("Part to check: ", start_string)

0e31a7752d7e6659329c3af6e5dbdc69c94ddc0d
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 16:15:04 +0800

    subtract the length of string, multiply result to '0' string, print the result

--- batch_7_string_manipulation/put_zeros_without_using_zfill().py
+++ batch_7_string_manipulation/put_zeros_without_using_zfill().py
@@ -1,8 +1,16 @@
 #Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
 #get string from user
-#multiply input number to the string '0'
+#subtract character count to the length of string
+#multiply result of subtract to the string '0'
 #display the result
 
 #get input from user
 input_string = input('Enter some text or numbers:')
-zero_input = int(input('How many zeros you want to add to the text?:'))
+zero_input = int(input('How many characters long you want the text to be?:'))
+
+#subtract the length of string to character count input
+output_string = zero_input - len(input_string)
+
+#print(input_string.zfill(zero_input))
+#add the zeros then display the result
+print(f'Text with zeros in the beginning:{'0' * output_string}{input_string}')

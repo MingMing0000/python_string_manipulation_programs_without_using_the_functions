5b7f390c70bab76b5ae9f4ed4b0b0a6f14f0efda
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 15:07:31 +0800

    pseudocode

---
+++ check_if_text_start_without_startswith().py
@@ -0,0 +1,4 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+#get input from user
+#check if the string start with the given string
+#display the result

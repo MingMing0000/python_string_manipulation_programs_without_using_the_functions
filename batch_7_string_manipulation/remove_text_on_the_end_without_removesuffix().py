3cf35ddb1de3911d913542599faaf9f0ce89d124
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 14:10:15 +0800

    pseudocode

---
+++ batch_7_string_manipulation/remove_text_on_the_end_without_removesuffix().py
@@ -0,0 +1,4 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#get the string from user 
+#remove the suffix by using for loop and len() then compare the string with the suffix
+#display the result

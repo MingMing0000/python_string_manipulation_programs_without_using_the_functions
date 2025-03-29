c8a8d2066d9ee0836b5d37898a31ecf4960837e7
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 14:40:32 +0800

    get and display user input

--- convert_to_uppercase_without_upper().py
+++ convert_to_uppercase_without_upper().py
@@ -2,3 +2,9 @@
 #get string from user
 #use ascii value to convert lower case to upper case
 #display the result
+
+# get string from user
+input_string = input("Enter a text to convert to uppercase: ")
+
+# display user input
+print("Original text:", input_string)

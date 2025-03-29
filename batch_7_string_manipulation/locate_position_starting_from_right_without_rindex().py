8c291b1ac862b02edb83580edc61cdb50a7aa574
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 17:46:15 +0800

    pseudocode

---
+++ batch_7_string_manipulation/locate_position_starting_from_right_without_rindex().py
@@ -0,0 +1,5 @@
+#Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+#get string from user
+#loop through the string
+#locate the position from right
+#print the result

11046b52e2defed73d88a4a808a8e0b7f01c1649
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 17:45:25 +0800

    edit comment

--- batch_7_string_manipulation/locate_position_of_1st_appearance_without_index().py
+++ batch_7_string_manipulation/locate_position_of_1st_appearance_without_index().py
@@ -1,6 +1,6 @@
 #Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
 #get string from user
-#loop through the string and add 1 each iteration to act as locator
+#loop through the string 
 #return the number count
 #display the result
 

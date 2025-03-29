46c34fa359f69cd01f50ed6f3a1b4c83b182b812
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2025-03-29 15:45:34 +0800

    get user input

--- batch_7_string_manipulation/put_zeros_without_using_zfill().py
+++ batch_7_string_manipulation/put_zeros_without_using_zfill().py
@@ -2,3 +2,7 @@
 #get string from user
 #multiply input number to the string '0'
 #display the result
+
+#get input from user
+input_string = input('Enter some text or numbers:')
+zero_input = int(input('How many zeros you want to add to the text?:'))
